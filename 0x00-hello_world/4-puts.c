cfa2477c5034c950583a63c8a65991894801e3cd
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-03-16 09:47:25 -0700

    5th - 1st

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Code for Success)
+*/
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	char str[55] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
 	return (0);
 }
