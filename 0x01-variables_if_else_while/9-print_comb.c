ea844e1dcc9bd405c63d760aa8aae4bc3fd7899d
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-03-17 13:50:27 -0700

    9th task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int	main(void)
+{
+	int	nbr;
+
+	nbr = 0;
+	while (nbr <= 9)
+	{
+		putchar(nbr + '0');
+		if (nbr != 9)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		nbr++;
+	}
+	putchar('\n');
+	return (0);
+}
