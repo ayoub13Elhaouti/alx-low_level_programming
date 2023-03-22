9b8f457d29473683ec099773006f2fb853b9cb83
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-03-21 17:22:54 -0700

    5 - 2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,17 +2,17 @@
 
 /**
  * print_sign - a function that prints the sign of a number
- * @nbr: number input
- * Return: 1 prints '+' if nbr > 0, 0 prints '0' if nbr = 0, -1 prints '-' if nbr < 0
+ * @n: number input
+ * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
  */
-int	print_sign(int	nbr)
+int	print_sign(int	n)
 {
-	if (nbr > 0)
+	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
 	}
-	else if (nbr == 0)
+	else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
