c84e2b3b4c5ae7017ebbda31f109a978237fe66d
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-03-21 18:20:01 -0700

    103 && 104 - 1st

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 98 Fibonacci numbers,
+ * starting with 1 and 2 (initial indexes)
+ * followed by a new line
+ * Return: ALways 0 (Success)
+ */
+int	main(void)
+{
+	unsigned long int	i, j, k, a, b, c, d;
+
+	j = 1;
+	k = 2;
+	printf("%lu", j);
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+	a = j / 1000000000;
+	b = j % 1000000000;
+	c = k / 1000000000;
+	d = k % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", c + (d / 1000000000));
+		printf("%lu", d % 1000000000);
+		c += a;
+		a = c - a;
+		d = d + b;
+		b = d - b;
+	}
+	printf("\n");
+	return (0);
+}
