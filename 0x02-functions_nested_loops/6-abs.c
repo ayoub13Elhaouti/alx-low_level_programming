82db5ac284a0757048537f26759495f7c3472c56
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-03-21 17:26:26 -0700

    6 - 2

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,9 @@
 /**
  * _abs - a function that computes the absolute value of an integer
  * @abs: integer input
- * Return: absolute value of abs
+ * Return: absolute value of ab
  */
-
-int	_abs(int	abs)
+int _abs(int abs)
 {
 	return (abs * ((abs > 0) - (abs < 0)));
 }
