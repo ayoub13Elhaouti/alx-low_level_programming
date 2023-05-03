b77749f67a3105572d8c90a6fa608d3ff21aaed8
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-05-03 03:07:48 +0100

    102nd Tasks - 1st try

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * _a - memory for an array of pointers reallocation
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list
+ * @new: new node to add to the list
+ *
+ * Return: the new list pointer
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newls;
+	size_t idx;
+
+	newls = malloc(size * sizeof(listint_t *));
+	if (newls == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (idx = 0; idx < size - 1; idx++)
+		newls[idx] = list[idx];
+	newls[idx] = new;
+	free(list);
+	return (newls);
+}
+
+/**
+ * free_listint_safe - free a listint_t linked list.
+ * @head: double pointer to the beginning of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t idx, nbr = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (head == NULL || *head == NULL)
+		return (nbr);
+	while (*head != NULL)
+	{
+		for (idx = 0; idx < nbr; idx++)
+		{
+			if (*head == list[idx])
+			{
+				*head = NULL;
+				free(list);
+				return (nbr);
+			}
+		}
+		nbr++;
+		list = _a(list, nbr, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (nbr);
+}
