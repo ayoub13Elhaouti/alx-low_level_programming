b84439ddca31e69bf6252eededd7640315f84a7c
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-05-05 00:57:57 +0100

    bit manipulation tasks - 2nd try

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int idx;
 
-	if (idx == 0 && index < 64)
+	if (n == 0 && index < 64)
 		return (0);
 
 	for (idx = 0; idx <= 63; n >>= 1, idx++)
