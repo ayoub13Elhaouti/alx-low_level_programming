9a7327168ba8285203f110217ec208c6b5a87232
Ayoub El Haouti <ayoub13elhaouti@gmail.com>
2023-05-09 03:24:18 +0100

    Mandatory tasks for I/O Files - 3rd try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,43 +11,43 @@
 
 int main(int argc, char *argv[])
 {
-int fl_from, fl_to;
-int nbr1 = 1024, nbr2 = 0;
+int file_from, file_to;
+int num1 = 1024, num2 = 0;
 char buffer[1024];
 
 if (argc != 3)
-	dprintf(STDERR_FILENO, "Usage: cp fl_from fl_to\n"), exit(97);
-fl_from = open(argv[1], O_RDONLY);
-if (fl_from == -1)
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 }
-fl_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 	| S_IRGRP | S_IWGRP | S_IROTH);
-if (fl_to == -1)
+if (file_to == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	close(fl_from), exit(99);
+	close(file_from), exit(99);
 }
-while (nbr1 == 1024)
+while (num1 == 1024)
 {
-	nbr1 = read(fl_from, buffer, 1024);
-	if (nbr1 == -1)
+	num1 = read(file_from, buffer, 1024);
+	if (num1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	nbr2 = write(fl_to, buffer, nbr1);
-	if (nbr2 < nbr1)
+	num2 = write(file_to, buffer, num1);
+	if (num2 < num1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
-if (close(fl_from) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fl_from), exit(100);
+if (close(file_from) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
-if (close(fl_to) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fl_to), exit(100);
+if (close(file_to) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
 return (0);
 }
